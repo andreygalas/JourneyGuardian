86297ddfa4e19126c868e3e6a2496d56b66f9b34
andreygalas <andreygalas2022@gmail.com>
2023-09-24 17:55:17 -0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_CRUD_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Trip.cs
@@ -0,0 +1,14 @@
+﻿namespace ASP.NET_CRUD_APP.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal StartingMiles { get; set; }
+        public decimal EndingMiles { get; set; }
+        public decimal TotalMiles { get; set; }
+        public decimal Stops { get; set; }
+        public decimal TotalPay { get; set;}
+
+    }
+}
