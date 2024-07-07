56bc5086a9ce1c6387f3bad16a0e52baad361e25
andreygalas <andreygalas2022@gmail.com>
2024-07-06 17:23:07 -0700

    updated trip model and ui

---
+++ Data/Migrations/20240707002108_updatingModel.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP.NET_CRUD_APP.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "BackHaul",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ChainUp",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "CrossDock",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DelayPay",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Doubles",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "HourlyPay",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PerDiem",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PostTrip",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PreTrip",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ShuttleStop",
+                table: "Trip",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BackHaul",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "ChainUp",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "CrossDock",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "DelayPay",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "Doubles",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "HourlyPay",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "PerDiem",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "PostTrip",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "PreTrip",
+                table: "Trip");
+
+            migrationBuilder.DropColumn(
+                name: "ShuttleStop",
+                table: "Trip");
+        }
+    }
+}
--- Models/Trip.cs
+++ Models/Trip.cs
@@ -8,10 +8,19 @@
         public decimal EndingMiles { get; set; }
         public decimal TotalMiles { get; set; }
         public decimal Stops { get; set; }
-        public decimal TotalPay { get; set;}
+        public decimal TotalPay { get; set; }
         public int Pallets { get; set; }
         public bool IsTeamRoute { get; set; }
         public decimal SuperStop { get; set; }
-
+        public decimal DelayPay { get; set; }
+        public decimal PerDiem { get; set; }
+        public decimal CrossDock { get; set; }
+        public decimal ShuttleStop { get; set; }
+        public decimal BackHaul { get; set; }
+        public decimal HourlyPay { get; set; }
+        public decimal ChainUp { get; set; }
+        public decimal Doubles { get; set; }
+        public decimal PreTrip { get; set; }
+        public decimal PostTrip { get; set; }
     }
 }
